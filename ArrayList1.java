2c223d1944f007a409f755a008e3315810ccb9dc
Mircea <105327253+MirceaGutium@users.noreply.github.com>
2022-06-27 16:41:42 +0300

    Add files via upload

---
+++ ArrayList1.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList1{
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Mr Green");
+        names.add("Mr Yellow");
+        names.add("Mr Red");
+        System.out.println (names.get(0));
+        System.out.println (names.get(1));
+        System.out.println (names.get(2));
+    }
+        }
